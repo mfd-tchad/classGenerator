0edab7bb95c024b5d25091734d193c3e3d9fcd0a
mfd <mfdewulf@gmail.com>
2018-09-28 14:57:33 +0200

    ajout du traducteur français dans main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QString locale = QLocale::system().name().section('_', 0, 0);
+    QTranslator translator;
+    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&translator);
+
     FenPrincipale fenetre;
     fenetre.show();
 
